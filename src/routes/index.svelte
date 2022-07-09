078666af26f792204c75dbd64d9886340402d3b3
Miguel Ferreira <miguel.personal@protonmail.com>
2022-07-09 01:51:39 +0100

    First commit 🚀

---
+++ src/routes/index.svelte
@@ -0,0 +1,45 @@
+<script>
+	/** 
+	 *
+	 * var quotes = <String, String>{
+	    "A vida é feita de quatro dias e meio": "4dias",
+	    "Analisa bem as situações. Trabalha para tudo o que pode ocorrer":
+	        "primeira",
+	    "Todos os que estão para lá são inimigos e querem deixar-te mal": "segunda",
+	    "Tens de pensar, não observar. Assim nunca mais fazes nada por ti":
+	        "terceira",
+	    "Sucesso é a soma de pequenos fracassos": "quarta",
+	    "Às vezes as coisas não nos parecem importantes, mas a qualquer momento podem ser":
+	        "quinta",
+	    "A melhor forma de ultrapassar os problemas é andando": "sexta",
+	    "Os idiotas são aqueles que pensam": "setima",
+	    "A vida é uma aventura, logo, qual é o stress?": "oitava",
+	  };
+	 */
+	
+	import { tick } from 'svelte';
+
+	const quotes = [
+		["A vida é feita de quatro dias e meio", "4dias"],
+		["Analisa bem as situações. Trabalha para tudo o que pode ocorrer", "primeira"],
+		["Todos os que estão para lá são inimigos e querem deixar-te mal", "segunda"],
+		["Tens de pensar, não observar. Assim nunca mais fazes nada por ti", "terceira"],
+		["Sucesso é a soma de pequenos fracassos", "quarta"],
+		["Às vezes as coisas não nos parecem importantes, mas a qualquer momento podem ser", "quinta"],
+		["A melhor forma de ultrapassar os problemas é andando", "sexta"],
+		["Os idiotas são aqueles que pensam", "setima"],
+		["A vida é uma aventura, logo, qual é o stress?", "oitava"]
+	];
+
+	let randomNumber = 0;
+
+	const intervalId = setInterval(() => {
+	  randomNumber = Math.floor(Math.random() * quotes.length);
+	  console.log("Random number: " + randomNumber);
+	}, 7000);	
+</script>
+
+<div class="w-screen h-screen flex flex-col space-y-10 items-center justify-center" style="background-image: url('/{quotes[randomNumber][1]}.png')">
+	<h1 class="text-2xl font-bold text-white">Já dizia o Jorge...</h1>
+	<p class="text-3xl font-bold text-white italic">"{quotes[randomNumber][0]}"</p>
+</div>
