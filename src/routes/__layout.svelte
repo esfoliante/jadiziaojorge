e44ed44cfca90201f1d75733bd1f429974aceda0
Miguel Ferreira <miguel.personal@protonmail.com>
2022-07-09 01:57:10 +0100

    feat: added svelte head

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -1,5 +1,16 @@
 <script>
   import "../app.css";
+
+  console.log("Heyyy... não devias estar a ver isto!!");
+  console.log("De qualquer das formas, olá! Pareces ser um ser humano bastante curioso...");
+  console.log("Envia-nos um e-mail para geral@avogg.pt para falarmos um bocadinho!");
 </script>
+
+<svelte:head>
+  <title>Já dizia o Jorge</title>
+  <meta name="author" content="Avogg">
+  <meta name="description" content="Umas pequenas palavras de inspiração do grande Jorge Vieira!">
+  <meta name="keywords" content="Jorge Vieira, Colégio de Gaia, jadiziaojorge, Já dizia o Jorge">
+</svelte:head>
 
 <slot />
